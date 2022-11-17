c5c2374c13d8e5613968b3628223e09497259c5f
Emilio Garcia <github@emiliogq.com>
2022-11-17 19:49:18 +0100

    Initial skeleton

---
+++ cws_heuristic.py
@@ -0,0 +1,57 @@
+from vrp_objects import Node, Edge, Route, Solution
+import math
+import operator
+
+vehCap = 100.0 # update vehicle capacity for each instance
+instanceName = 'A-n80-k10' # name of the instance
+# txt file with the VRP instance data (nodeID, x, y, demand)
+fileName = 'data/instanceName' + '_input_nodes.txt'
+
+with open(fileName) as instance:
+    i = 0
+    nodes = []
+    for line in instance:
+        # array data with node data: x, y demand
+        data = [float(x) for x in line.split()]
+        aNode = Node(i, data[0], data[1], data[2])
+        nodes.append(aNode)
+        i += 1
+
+depot = nodes[0] # node 0 is depot
+for node in nodes[1:]: # exclude depot
+    dnEdge = Edge(depot, node) # creates (depot, node) edge
+    ndEdge = Edge(node, depot)
+    dnEdge.invEdge = ndEdge # sets the inverse edge
+    ndEdge.invEdge = dnEdge
+    # compute the Euclidean Distance as cost
+    dnEdge.cost = math.sqrt((node.x - depot.x)**2 + (node.y - depot.y)**2)
+    ndEdge.cost = dnEdge.cost # assume symetric costs
+    # save in node a reference to the (depot, node) edge
+    node.dnEdge = dnEdge
+    node.ndEdge = ndEdge
+
+savingsList = []
+for i in range(1, len(nodes) - 1): # excludes depot
+    iNode = nodes[i]
+    for j in range(i + 1, len(nodes)):
+        jNode = nodes[j]
+        ijEdge = Edge(iNode, jNode) # creates the (i, j) edge
+        jiEdge = Edge(jNode, iNode)
+        ijEdge.invEdge = jiEdge # sets the inverse edge
+        jiEdge.invEdge = ijEdge
+        # compute the Euclidean distance as cost
+        ijEdge.cost = math.sqrt((jNode.x - iNode.x)**2 + (jNode.y - iNode.y)**2)
+        jiEdge.cost = ijEdge.cost # assume symmectir costs
+        # compute savins as proposed by Clark & Wright
+        ijEdge.savings = iNode.ndEdge.cost + jNode.dnEdge.cost - ijEdge.cost
+        jiEdge.savings = ijEdge.savings
+        # save one edge in the savings list
+        savingsList.append(ijEdge)
+
+
+
+savingsList.sort(key = operator.attrgetter("savings"), reverse = True)
+# TODO creo que el ej 2 iria aqui haciendo orden random con una exponencial o triangular
+
+sol = Solution()
+#### ...
---
+++ vrp_objects.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init_(self, ID, x, y, demand):
+        self.Id = ID # node identifier (depot ID = 0)
+        self.x = x # Euclidean x-coordinate
+        self.y = y # Euclidean y-coordinate
+        self.demand = demand # demand (is 0 for depot and positive for others)
+        self.inRoute = None # route to which node belongs
+        self.isInterior = False # an interior node is not connected to depot
+        self.dnEdge = None # edge (arc) from depot to this node
+        self.ndEdge = None # edge (arc) from this node to depot
+
+class Edge:
+    def __init_(self, origin, end):
+        self.origin = origin # origin node of the edge (arc)
+        self.end = end # end node of the edge (arc)
+        self.cost = 0.0 # edge cost
+        self.savings = 0.0 # edge savings (Clarke & Wright)
+        self.invEdge = None # inverse edge (arc)
+
+class Route:
+    def __init__(self):
+        self.cost = 0.0 # cost of this route
+        self.edges = [] # sorted edges in this route
+        self.demand = 0.0 # total demand covered by this route
+    
+    def reverse(self): # e.g. 0 -> 2 -> 6 -> 0 becomes 0 -> 6 -> 2 -> 0
+        size = len(self.edges)
+        for i in range(size):
+            edge = self.edges[i]
+            invEdge = edge.invEdge
+            self.edges.remove(edge)
+            self.edges.insert(0, invEdge)
+
+class Solution:
+    last_ID = -1 #counts the number of solutions, starts with 0
+    def __init__(self):
+        Solution.last_ID += 1
+        self.ID = Solution.last_ID
+        self.routes = [] # routes in this solution
+        self.cost = 0.0 # cost of this solution
+        self.demand = 0.0 # total demand covered by this solution
